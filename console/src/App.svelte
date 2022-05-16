f8123ea8cab8991d815dd96b6e5d27c766e1a2a5
Simon Ordish <simon.ordish@masagi.co.uk>
2022-05-16 20:17:01 +0200

    Added linting and Bulma CSS

--- console/src/App.svelte
+++ console/src/App.svelte
@@ -12,21 +12,65 @@
   //   name = data;
   // });
 
-  import { Router, Route, Link } from "svelte-routing";
-  import Home from "./routes/home.svelte";
-  import Settings from "./routes/settings.svelte";
+  import { Router, Route, Link } from 'svelte-routing'
+  import Home from './routes/home.svelte'
+  import Settings from './routes/settings.svelte'
 
-  export let url = "";
+  export let url = ''
+
+  const src = 'taal-logo.png'
 </script>
 
+<svelte:head>
+  <!-- <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" /> -->
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+  />
+</svelte:head>
+
 <Router {url}>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/	settings">Settings</Link>
+  <nav
+    class="navbar is-fixed-top has-background-dark"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a class="navbar-item has-text-light" href="https://taal.com">
+        <img {src} alt="Taal logo" />
+        <span class="is-size-4 client">Client</span>
+      </a>
+
+      <a
+        href="#/"
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <Link to="/" class="navbar-item has-text-light">Home</Link>
+
+        <Link to="/settings" class="navbar-item has-text-light">Settings</Link>
+      </div>
+    </div>
   </nav>
 
-  <div>
+  <main class="section">
     <Route path="/"><Home /></Route>
     <Route path="settings" component={Settings} />
-  </div>
+  </main>
 </Router>
+
+<style>
+  .client {
+    margin-left: 5px;
+  }
+</style>
--- console/src/routes/settings.svelte
+++ console/src/routes/settings.svelte
@@ -1,3 +1,76 @@
-<form>
-  <input type="text" name="dburl" placeholder="Enter database URL" />
-</form>
+<div class="field">
+  <label for="text1" class="label">Name</label>
+  <div id="text1" class="control">
+    <input class="input" type="text" placeholder="Text input" />
+  </div>
+</div>
+
+<div class="field">
+  <label class="label" for="email">Email</label>
+  <div class="control has-icons-left has-icons-right">
+    <input
+      id="email"
+      class="input is-danger"
+      type="email"
+      placeholder="Email input"
+      value="hello@"
+    />
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope" />
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle" />
+    </span>
+  </div>
+  <p class="help is-danger">This email is invalid</p>
+</div>
+
+<div class="field">
+  <label class="label" for="drop">Subject</label>
+  <div class="control">
+    <div id="drop" class="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label" for="msg">Message</label>
+  <div class="control">
+    <textarea id="msg" class="textarea" placeholder="Textarea" />
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox" />
+      I agree to the <a href="#/">terms and conditions</a>
+    </label>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="question" />
+      Yes
+    </label>
+    <label class="radio">
+      <input type="radio" name="question" />
+      No
+    </label>
+  </div>
+</div>
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+  <div class="control">
+    <button class="button is-link is-light">Cancel</button>
+  </div>
+</div>
