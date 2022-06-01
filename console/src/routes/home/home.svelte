8fcbec9746c642cac711fd70888eca4f7f7f6041
Michael Boeckli <michael.boeckli@taal.com>
2022-06-01 13:23:34 +0200

    Show size of transaction data

--- console/src/routes/home/home.svelte
+++ console/src/routes/home/home.svelte
@@ -4,6 +4,16 @@
 </script>
 
 <main>
+    <h1>Status</h1>
     <Health/>
+    <h1>Transaction stats</h1>
     <TransactionStats/>
 </main>
+
+<style>
+  h1 {
+    font-size: 1.4em;
+    font-weight: bold;
+    display: block;
+  }
+</style>
--- console/src/routes/home/transaction_stats.svelte
+++ console/src/routes/home/transaction_stats.svelte
@@ -1,18 +1,26 @@
 <script>
   import { onMount } from 'svelte'
+  import {TxDataSize} from '../tx_history/transaction_format_functions.svelte'
   let transactions
   let nrOfTransactions = 0
+  let combinedSize = 0.0
 
   onMount(async () => {
     await fetch(`${BASE_URL}/api/v1/transactions/info`)
       .then((r) => r.json())
       .then((data) => {
         transactions = data.transactions
-        nrOfTransactions = transactions.unshift(0)
+        nrOfTransactions = transactions.length
+
+        var dataSizes = transactions.map((a) => a.data_bytes)
+        dataSizes.forEach(element => {
+          combinedSize += element
+        });
       })
   })
 </script>
 
 <div class="field">
   <h1>Total number of transactions: {nrOfTransactions}</h1>
+  <h1>Total size of transaction data: {TxDataSize(combinedSize)}</h1>
 </div>
