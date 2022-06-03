142cb3ea400b6176ab4b1513f911930d740cd73c
Michael Boeckli <michael.boeckli@taal.com>
2022-06-03 14:58:05 +0200

    Reuse transactions info functionality in home screen

--- console/src/routes/home/transaction_chart.svelte
+++ console/src/routes/home/transaction_chart.svelte
@@ -1,18 +1,56 @@
 <script>
   import chartjs from 'chart.js'
+  import { onMount } from 'svelte'
 
-  let valueLabels = {}
   export let transactions
-  let reverseTxs
 
   let chartValues = []
   let chartLabels = []
+
+  let initialChartValues
+  let initialChartLabels
 
   let ctx
   let chart
   let chartCanvas
   function unique(value, index, self) {
     return self.indexOf(value) === index
+  }
+
+  const options = {
+    responsive: true,
+    title: {
+      display: false,
+      text: 'Transaction history',
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            suggestedMin: 0,
+            suggestedMax: 7,
+            fontSize: fontSize,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of transactions',
+            fontSize: fontSize,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          type: 'time',
+          distribution: 'linear',
+          time: {
+            unit: 'day',
+          },
+          ticks: {
+            fontSize: fontSize,
+          },
+        },
+      ],
+    },
   }
 
   const fontSize = 18
@@ -22,78 +60,70 @@
     return date.toISOString().split('T')[0]
   }
 
-  $: if (transactions.length > 0) {
-    reverseTxs = transactions.reverse() // Order of time axis is ascending
+  onMount(() => {
+    var today = new Date()
+    var tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
 
-    chartLabels = reverseTxs
-      .map((tx) => GetDateFromISODateString(tx.created_at))
-      .filter(unique)
-
-    chartLabels.forEach((element) => {
-      valueLabels[element] = 0
-    })
-
-    reverseTxs.forEach(
-      (tx) => valueLabels[GetDateFromISODateString(tx.created_at)]++
-    )
-    chartValues = Object.values(valueLabels)
-
-    const options = {
-      responsive: true,
-      title: {
-        display: false,
-        text: 'Transaction history',
-      },
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              suggestedMin: 0,
-              suggestedMax: 7,
-              fontSize: fontSize,
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Number of transactions',
-              fontSize: fontSize,
-            },
-          },
-        ],
-        xAxes: [
-          {
-            type: 'time',
-            distribution: 'linear',
-            time: {
-              unit: 'day',
-            },
-            ticks: {
-              fontSize: fontSize,
-            },
-          },
-        ],
-      },
-    }
+    initialChartLabels = [
+      GetDateFromISODateString(today),
+      GetDateFromISODateString(tomorrow),
+    ]
+    initialChartValues = [0, 0]
 
     const config = {
       type: 'bar',
       options: options,
       data: {
-        labels: chartLabels,
+        labels: initialChartLabels,
         datasets: [
           {
             label: 'Transactions per day',
             borderColor: 'rgb(255, 99, 132)',
             lineTension: '0.2',
-            data: chartValues,
+            data: initialChartValues,
             fill: false,
           },
         ],
       },
     }
-
     ctx = chartCanvas.getContext('2d')
     chart = new chartjs(ctx, config)
+  })
+
+  function OnTransactionsChange(transactions) {
+    if (transactions == null) {
+      return
+    }
+
+    chartLabels = initialChartLabels
+    chartValues = initialChartValues
+
+    if (transactions.length > 0) {
+      var reverseTxs = []
+      var valueLabels = {}
+      reverseTxs = transactions.reverse() // Order of time axis is ascending
+
+      chartLabels = reverseTxs
+        .map((tx) => GetDateFromISODateString(tx.created_at))
+        .filter(unique)
+
+      chartLabels.forEach((element) => {
+        valueLabels[element] = 0
+      })
+
+      reverseTxs.forEach(
+        (tx) => valueLabels[GetDateFromISODateString(tx.created_at)]++
+      )
+      chartValues = Object.values(valueLabels)
+    }
+
+    chart.data.labels = chartLabels
+    chart.data.datasets[0].data = chartValues
+    chart.update()
   }
+
+  $: OnTransactionsChange(transactions)
 </script>
 
 <canvas bind:this={chartCanvas} id="transactionsChart" />
--- console/src/routes/home/transaction_stats.svelte
+++ console/src/routes/home/transaction_stats.svelte
@@ -1,27 +1,34 @@
 <script>
-  import { onMount } from 'svelte'
   import { TxDataSize } from '../tx_history/transaction_format_functions.svelte'
   import TransactionChart from './transaction_chart.svelte'
 
-  let transactions = []
+  import TransactionsInfo from '../util/transactions_info.svelte'
+
+  let transactions
   let nrOfTransactions = 0
   let combinedSize = 0
 
-  onMount(async () => {
-    await fetch(`${BASE_URL}/api/v1/transactions/info`)
-      .then((r) => r.json())
-      .then((data) => {
-        transactions = data.transactions
-        nrOfTransactions = transactions.length
-        var dataSizes = transactions.map((tx) => tx.data_bytes)
-        dataSizes.forEach((element) => {
-          combinedSize += element
-        })
-      })
-  })
+  function OnTransactionsChange(transactions) {
+    if (transactions == null) {
+      return
+    }
+    nrOfTransactions = transactions.length
+    var dataSizes = transactions.map((tx) => tx.data_bytes)
+
+    if (dataSizes.length == 0) {
+      combinedSize = 0
+    }
+
+    dataSizes.forEach((element) => {
+      combinedSize += element
+    })
+  }
+
+  $: OnTransactionsChange(transactions)
 </script>
 
 <div class="field">
+  <TransactionsInfo bind:transactions />
   <h1>Total number of transactions: {nrOfTransactions}</h1>
   <h1>Total size of transaction data: {TxDataSize(combinedSize)}</h1>
   <TransactionChart bind:transactions />
