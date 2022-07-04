5872523bb5869fe191fe0e237112ec0886fffc39
Simon Ordish <simon.ordish@masagi.co.uk>
2022-07-05 00:58:22 +0200

    --trimpath

--- build.sh
+++ build.sh
@@ -49,12 +49,12 @@ mkdir -p build/darwin
 mkdir -p build/windows
 mkdir -p build/linux
 
-env GOOS=darwin GOARCH=amd64 go build -o build/darwin/${PROG_NAME}_intel -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
-env GOOS=darwin GOARCH=arm64 go build -o build/darwin/${PROG_NAME}_arm -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env GOOS=darwin GOARCH=amd64 go build --trimpath -o build/darwin/${PROG_NAME}_intel -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env GOOS=darwin GOARCH=arm64 go build --trimpath -o build/darwin/${PROG_NAME}_arm -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 lipo -create -output build/darwin/$PROG_NAME build/darwin/${PROG_NAME}_intel build/darwin/${PROG_NAME}_arm
 
-env GOOS=linux GOARCH=amd64 go build -o build/linux/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
-env GOOS=windows GOARCH=386 go build -o build/windows/$PROG_NAME.exe -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env GOOS=linux GOARCH=amd64 go build --trimpath -o build/linux/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env GOOS=windows GOARCH=386 go build --trimpath -o build/windows/$PROG_NAME.exe -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 
 if [[ "$?" == "0" ]]; then
   echo $GIT_COMMIT > build/commit.dat
