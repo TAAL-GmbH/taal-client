597f1e41f1608c0c3d99d39cf63349ed5f0f5eee
Simon Ordish <simon.ordish@masagi.co.uk>
2022-07-08 12:51:21 +0200

    New build script for jenkins

--- build.sh
+++ build.sh
@@ -49,12 +49,38 @@ mkdir -p build/darwin
 mkdir -p build/windows
 mkdir -p build/linux
 
-env GOOS=darwin GOARCH=amd64 go build --trimpath -o build/darwin/${PROG_NAME}_intel -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
-env GOOS=darwin GOARCH=arm64 go build --trimpath -o build/darwin/${PROG_NAME}_arm -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build --trimpath -o build/darwin/${PROG_NAME}_intel -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build --trimpath -o build/darwin/${PROG_NAME}_arm -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 lipo -create -output build/darwin/$PROG_NAME build/darwin/${PROG_NAME}_intel build/darwin/${PROG_NAME}_arm
 
-env GOOS=linux GOARCH=amd64 go build --trimpath -o build/linux/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
-env GOOS=windows GOARCH=386 go build --trimpath -o build/windows/$PROG_NAME.exe -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build --trimpath -o build/linux/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+
+env CGO_ENABLED=1 GOOS=windows GOARCH=386 go build --trimpath -o build/windows/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+
+# # env GOOS=windows GOARCH=386 go build --trimpath -o build/windows/$PROG_NAME.exe -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+# ssh root@138.201.123.88 << EOL
+#   cd taal-client
+#   git reset --hard
+#   git pull ordishs master
+
+#   rm -f taal-client
+#   rm -f taal-client.exe
+
+#   cd console
+#   npm i
+#   npm run build
+#   cd ..
+
+#   CGO_ENABLED=1 /root/go/bin/go build --trimpath -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+
+#   CGO_ENABLED=1 GOOS=windows GOARCH=386 CC="i686-w64-mingw32-gcc -fno-stack-protector -D_FORTIFY_SOURCE=0 -lssp" /root/go/bin/go build  --trimpath -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+# EOL
+
+# scp root@138.201.123.88:./taal-client/taal-client build/linux/
+# scp root@138.201.123.88:./taal-client/taal-client.exe build/windows/
+# cd build/windows
+# jar cvf taal-client.zip ./taal-client.exe
+# cd ../..
 
 if [[ "$?" == "0" ]]; then
   echo $GIT_COMMIT > build/commit.dat
