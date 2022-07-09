3d63e5d573dc711f1c58af068173e5aaab0fe816
Simon Ordish <simon.ordish@masagi.co.uk>
2022-07-10 01:25:09 +0200

    Build darwin remotely

--- build.sh
+++ build.sh
@@ -47,18 +47,36 @@ npm run build
 cd ..
 
 
-mkdir -p build/darwin/intel
-mkdir -p build/darwin/arm
+mkdir -p build/darwin
 mkdir -p build/windows
 mkdir -p build/linux
 
-env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build --trimpath -o build/darwin/intel/${PROG_NAME} -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
-env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build --trimpath -o build/darwin/arm/${PROG_NAME} -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+#env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build --trimpath -o build/darwin/intel/${PROG_NAME} -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+#env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build --trimpath -o  -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 #lipo -create -output build/darwin/$PROG_NAME build/darwin/intel/${PROG_NAME} build/darwin/arm/${PROG_NAME}
+ssh taal@www.masa.gi << EOL
+  cd taal-client
+  git reset --hard
+  git pull
+
+  rm -rf build
+  mkdir -p build/darwin/intel
+  mkdir -p build/darwin/arm
+
+  cd console
+  npm i
+  npm run build
+  cd ..
+
+  env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 /opt/homebrew/bin/go build --trimpath  -o build/${PROG_NAME}_amd64 -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+  env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 /opt/homebrew/bin/go build --trimpath  -o build/${PROG_NAME}_arm64 -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
+  lipo -create -output build/$PROG_NAME build/${PROG_NAME}_amd64 build/${PROG_NAME}_arm64
+EOL
+
+scp taal@www.masa.gi:./taal-client/build/* ./build/darwin 
 
 env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build --trimpath -o build/linux/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 
-# env CGO_ENABLED=1 GOOS=windows GOARCH=386 go build --trimpath -o build/windows/$PROG_NAME -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 env CGO_ENABLED=1 GOOS=windows GOARCH=386 CC="i686-w64-mingw32-gcc -fno-stack-protector -D_FORTIFY_SOURCE=0 -lssp" go build  --trimpath  -o build/windows/${PROG_NAME}.exe -ldflags="-s -w -X main.commit=${GIT_COMMIT}"
 cd build/windows
 jar cvf taal-client.zip ./taal-client.exe
